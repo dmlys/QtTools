c3b2f1b4ff3058386f91cdf21d42d2e4bfa8f5fa
Dmitry Lisachenko <xdemonx07@gmail.com>
2019-04-01 23:38:41 +0300

    sftree_facade_qtbase: QModelIndex row checks, return default empty on invalid

--- include/viewed/sftree_facade_qtbase.hpp
+++ include/viewed/sftree_facade_qtbase.hpp
@@ -30,7 +30,6 @@
 #include <boost/multi_index/hashed_index.hpp>
 #include <boost/multi_index/random_access_index.hpp>
 #include <boost/iterator/transform_iterator.hpp>
-//#include <boost/variant.hpp>
 
 #include <QtCore/QAbstractItemModel>
 #include <QtTools/ToolsBase.hpp>
@@ -239,12 +238,13 @@ namespace viewed
 			node_type        node;             // node data
 
 			page_type(const self_type * self) : children(create_container(self)) {}
+			page_type(ext::noinit_type val)   : children(create_container(val))  {}
 		};
 
 		struct get_children_type
 		{
 			using result_type = const ivalue_container &;
-			result_type operator()(const leaf_type & leaf) const { return ms_empty_container; }
+			result_type operator()(const leaf_type & leaf) const { return ms_empty_page.children; }
 			result_type operator()(const page_type & page) const { return page.children; }
 			result_type operator()(const ivalue_ptr & val) const { return viewed::visit(*this, val); }
 
@@ -345,7 +345,8 @@ namespace viewed
 
 	protected:
 		static const pathview_type     ms_empty_path;
-		static const ivalue_container  ms_empty_container;
+		static const page_type         ms_empty_page; // alignas(std::max_align_t) applied on definition
+		static const ivalue_ptr        ms_empty_ivalue_ptr;
 
 	protected:
 		// our statefull traits
@@ -524,11 +525,14 @@ namespace viewed
 	/*                   class statics and page creation                    */
 	/************************************************************************/
 	template <class Traits, class ModelBase>
-	const typename sftree_facade_qtbase<Traits, ModelBase>::ivalue_container sftree_facade_qtbase<Traits, ModelBase>::ms_empty_container
-		= sftree_facade_qtbase<Traits, ModelBase>::create_container(ext::noinit);
+	const typename sftree_facade_qtbase<Traits, ModelBase>::pathview_type sftree_facade_qtbase<Traits, ModelBase>::ms_empty_path;
 
 	template <class Traits, class ModelBase>
-	const typename sftree_facade_qtbase<Traits, ModelBase>::pathview_type sftree_facade_qtbase<Traits, ModelBase>::ms_empty_path;
+	alignas(std::max_align_t) const typename sftree_facade_qtbase<Traits, ModelBase>::page_type sftree_facade_qtbase<Traits, ModelBase>::ms_empty_page(ext::noinit);
+
+	template <class Traits, class ModelBase>
+	const typename sftree_facade_qtbase<Traits, ModelBase>::ivalue_ptr sftree_facade_qtbase<Traits, ModelBase>::ms_empty_ivalue_ptr = &ms_empty_page;
+
 
 	template <class Traits, class ModelBase>
 	typename sftree_facade_qtbase<Traits, ModelBase>::ivalue_container sftree_facade_qtbase<Traits, ModelBase>::create_container(const self_type * self)
@@ -742,8 +746,11 @@ namespace viewed
 		assert(page);
 
 		auto & seq_view = page->children.template get<by_seq>();
-		assert(index.row() < get_children_count(page));
-		return seq_view[index.row()];
+		//assert(index.row() < get_children_count(page));
+		if (index.row() < get_children_count(page))
+			return seq_view[index.row()];
+		else
+			return ms_empty_ivalue_ptr;
 	}
 
 	template <class Traits, class ModelBase>
